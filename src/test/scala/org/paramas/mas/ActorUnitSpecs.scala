74a0da0b0f06e6646576dfda8b0e5cdb15cca819
Daniel Krzywicki <eleaar@gmail.com>
2014-04-14 16:35:59 +0200

    First tests for reaper

--- src/test/scala/org/paramas/mas/ActorUnitSpecs.scala
+++ src/test/scala/org/paramas/mas/ActorUnitSpecs.scala
@@ -4,11 +4,8 @@ import akka.testkit.{ImplicitSender, TestKit}
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 import akka.actor.ActorSystem
 
-/**
- * Created by krzywick on 2014-04-14.
- */
 abstract class ActorUnitSpecs(_system: ActorSystem) extends TestKit(_system) with ImplicitSender
-with WordSpecLike with Matchers with BeforeAndAfterAll{
+with WordSpecLike with Matchers with BeforeAndAfterAll {
 
   override def afterAll {
     TestKit.shutdownActorSystem(system)
--- src/test/scala/org/paramas/mas/util/ReaperSpecs.scala
+++ src/test/scala/org/paramas/mas/util/ReaperSpecs.scala
@@ -1,8 +1,47 @@
 package org.paramas.mas.util
 
-/**
- * Created by krzywick on 2014-04-14.
- */
-class ReaperSpecs {
 
+import akka.actor.ActorSystem
+import akka.actor.Actor
+import akka.actor.Props
+import akka.testkit.TestKit
+import org.paramas.mas.ActorUnitSpecs
+
+class ReaperSpecs extends ActorUnitSpecs(ActorSystem("ReaperSpecs")) {
+
+  "A Reaper actor" should {
+      "terminate early when given an empty initial list of souls" in {
+        fail
+      }
+
+      "terminate early when given an initial list of already terminated souls" in {
+
+      }
+
+      "ignore terminations from unknown souls" in {
+             fail
+      }
+
+      "terminate when all souls are terminated" in {
+        fail
+      }
+  }
+
+  "A Promise Reaper" should {
+    "complete the promise when terminated" in {
+      fail
+    }
+  }
+
+//  "An Echo actor" must {
+//
+//    "send back messages unchanged" in {
+//      val echo = system.actorOf(Props[EchoActor])
+//      echo ! "hello world"
+//      expectMsg("hello world")
+//    }
+//
+//  }
 }
+
+
